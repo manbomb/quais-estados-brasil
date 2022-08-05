7d4060ca98693e4ff61b3a921ef6844a6c350116
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-08-05 09:51:20 -0300

    add error to input

--- src/components/inputText/InputText.jsx
+++ src/components/inputText/InputText.jsx
@@ -1,12 +1,12 @@
 import styles from './InputText.module.css';
 
-const InputText = ({ value, onChange, className = '', onEnter, ...props }) => {
+const InputText = ({ value, onChange, className = '', onEnter, error = false, ...props }) => {
     const onKeyDown = (ev) => {
         if (ev.code === 'Enter') onEnter(ev);
     };
 
     return <div className={`${styles.container} ${className}`} {...props}>
-        <input type={'text'} onChange={ev => onChange(ev.target.value)} value={value} onKeyDown={onKeyDown} />
+        <input type={'text'} className={`${error ? styles.error : ''}`} onChange={ev => onChange(ev.target.value)} value={value} onKeyDown={onKeyDown} />
     </div>;
 };
 
