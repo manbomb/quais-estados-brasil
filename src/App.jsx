52ef7594df9c58819ce5255a6b46d9543da8bf93
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-08-05 09:52:20 -0300

    implements first behavior

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,52 @@ import styles from './App.module.css';
 import BrazilMap from './components/brazilMap/BrazilMap';
 import InputText from './components/inputText/InputText';
 
+import data from './data/estados.json';
+import { removeAcento } from './utils';
+
 const App = () => {
 
     const [estado, setEstado] = useState('');
+    const [selectStates, setSelectedStates] = useState([]);
+    const [error, setError] = useState(false);
+
+    const enterNewEstado = () => {
+        if (!estado) return;
+        const estados = data.estados;
+
+        const estadoLower = removeAcento(estado.toLowerCase());
+        const estadosFiltred = estados.filter(e => {
+            const eNameLower = removeAcento(e.nome.toLowerCase());
+            if (eNameLower === estadoLower) return true;
+            return false;
+        });
+
+        if (estadosFiltred.length < 1) {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+        const estadoFound = estadosFiltred[0];
+        const newSelected = [...selectStates].filter(e => e.sigla !== estadoFound.sigla);
+        newSelected.push(estadoFound);
+        setSelectedStates(newSelected);
+
+        setEstado('');
+    };
 
     return <div className={styles.container}>
         <div className={styles.list}>
-            <InputText onChange={setEstado} value={estado} onEnter={() => {}} />
+            <InputText
+                onChange={setEstado}
+                value={estado}
+                onEnter={() => enterNewEstado()}
+                error={error}
+            />
         </div>
         <div className={styles.map}>
-            <BrazilMap />
+            <BrazilMap selectStates={selectStates.map(e => e.sigla.toLowerCase())} />
         </div>
     </div>;
 };
